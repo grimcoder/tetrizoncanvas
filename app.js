5e6c939c05089ebed49c2215246e5a34682e6c95
Taras Kovtun <grimcoder@users.noreply.github.com>
2015-10-27 16:01:47 -0700

    started implementing figure rotation

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ document.write("<canvas id='mainCanvas' width='400px' height='800px '  style='bo
 var canvas = document.getElementById("mainCanvas");
 var context = canvas.getContext("2d");
 var cellSize = 40;
+var tick = 1000;
 var droppedCells = [];
+var activeFigure = [
+    [0, 1],
+    [1, 1],
+    [1, 2],
+    [1, 3]
+];
+var activePosition = [5, 5];
 document.onkeydown = function (evt) {
     var evt = evt || window.event;
     switch (evt.keyCode) {
@@ -22,8 +30,6 @@ document.onkeydown = function (evt) {
             console.log(evt.keyCode);
     }
 };
-context.translate(0, canvas.height);
-context.scale(1, -1);
 droppedCells.push([0, 0]);
 droppedCells.push([1, 0]);
 droppedCells.push([2, 0]);
@@ -36,7 +42,19 @@ droppedCells.push([7, 1]);
 droppedCells.push([9, 0]);
 droppedCells.push([9, 1]);
 droppedCells.push([9, 2]);
-for (var tuple in droppedCells) {
-    context.fillRect(droppedCells[tuple][0] * cellSize, droppedCells[tuple][1] * cellSize, cellSize, cellSize);
+function drawCanvas() {
+    context.save();
+    context.translate(0, canvas.height);
+    context.scale(1, -1);
+    for (var tuple in droppedCells) {
+        context.fillRect(droppedCells[tuple][0] * cellSize, droppedCells[tuple][1] * cellSize, cellSize, cellSize);
+    }
+    for (var tuple in activeFigure) {
+        context.fillRect((activePosition[0] + activeFigure[tuple][0]) * cellSize, (activePosition[1] + activeFigure[tuple][1]) * cellSize, cellSize, cellSize);
+    }
+    context.restore();
 }
+setInterval(function () {
+    drawCanvas();
+}, tick);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,53 +1,75 @@
 document.write("<canvas id='mainCanvas' width='400px' height='800px '  style='border:1px solid #000000;'></canvas>");
 
-var canvas   = <HTMLCanvasElement> document.getElementById("mainCanvas");
-var context = canvas.getContext("2d");
+var canvas   = <HTMLCanvasElement> document.getElementById("mainCanvas")
+
+var context = canvas.getContext("2d")
 var cellSize: number = 40
-var droppedCells:  Array< [number, number]> = [];
+var tick: number = 1000
+var droppedCells:  Array< [number, number]> = []
+
+var activeFigure:  Array< [number, number]> = [
+    [0,1],
+    [1,1],
+    [1,2],
+    [1,3]
+]
+
+var activePosition : [number, number] = [5,5]
 
 
 document.onkeydown = (evt)=> {
-    var evt = evt || window.event;
+    var evt = evt || window.event
 
     switch (evt.keyCode) {
-
         case 37:
-            console.log("left");
+            console.log("left")
             break;
         case 38:
-            console.log("rotate");
+            console.log("rotate")
             break;
         case 39:
-            console.log("right");
+            console.log("right")
             break;
         case 40:
-            console.log("down");
+            console.log("down")
             break;
         default:
             console.log(evt.keyCode)
-
     }
 };
 
-context.translate(0, canvas.height);
-context.scale(1, -1);
+droppedCells.push([0,0])
+droppedCells.push([1,0])
+droppedCells.push([2,0])
+droppedCells.push([3,0])
+droppedCells.push([4,0])
+droppedCells.push([5,0])
+droppedCells.push([6,0])
+droppedCells.push([7,0])
+droppedCells.push([7,1])
+droppedCells.push([9,0])
+droppedCells.push([9,1])
+droppedCells.push([9,2])
 
+function drawCanvas(){
+    context.save()
+    context.translate(0, canvas.height)
+    context.scale(1, -1);
 
-droppedCells.push([0,0]);
-droppedCells.push([1,0]);
-droppedCells.push([2,0]);
-droppedCells.push([3,0]);
-droppedCells.push([4,0]);
-droppedCells.push([5,0]);
-droppedCells.push([6,0]);
-droppedCells.push([7,0]);
-droppedCells.push([7,1]);
-droppedCells.push([9,0]);
-droppedCells.push([9,1]);
-droppedCells.push([9,2]);
+    for (var tuple in droppedCells){
+        context.fillRect(droppedCells[tuple][0] * cellSize, droppedCells[tuple][1]*cellSize, cellSize, cellSize)
+    }
 
-for (var tuple in droppedCells){
-    context.fillRect(droppedCells[tuple][0] * cellSize, droppedCells[tuple][1]*cellSize, cellSize, cellSize);
+    for (var tuple in activeFigure){
+        context.fillRect((activePosition[0] +  activeFigure[tuple][0]) * cellSize, (activePosition[1] + activeFigure[tuple][1])*cellSize, cellSize, cellSize)
+    }
+
+    context.restore()
 }
 
+setInterval(()=>{
 
+        drawCanvas()
+
+    }
+    ,tick);
