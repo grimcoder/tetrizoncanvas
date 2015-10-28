fec46d308af7985d44b64f7b234067abe54005db
grimcoder <taraskovtun@gmail.com>
2015-10-28 14:40:58 -0700

    added figures matrix

--- app.js
+++ app.js
@@ -5,26 +5,21 @@ var Orientation;
     Orientation[Orientation["Down"] = 2] = "Down";
     Orientation[Orientation["Left"] = 3] = "Left";
 })(Orientation || (Orientation = {}));
+var Figure;
+(function (Figure) {
+    Figure[Figure["El"] = 0] = "El";
+    Figure[Figure["BackEl"] = 1] = "BackEl";
+    Figure[Figure["Cube"] = 2] = "Cube";
+    Figure[Figure["Stick"] = 3] = "Stick";
+    Figure[Figure["Gee"] = 4] = "Gee";
+    Figure[Figure["BackGee"] = 5] = "BackGee";
+    Figure[Figure["Piramide"] = 6] = "Piramide";
+})(Figure || (Figure = {}));
 var Figures = (function () {
     function Figures() {
     }
     return Figures;
 })();
-document.write("<canvas id='mainCanvas' width='400px' height='800px '  style='border:1px solid #000000;'></canvas>");
-var canvas = document.getElementById("mainCanvas");
-var context = canvas.getContext("2d");
-var cellSize = 40;
-var width = 10;
-var heigth = 20;
-var tick = 1000;
-var droppedCells = [];
-var activeFigure = [
-    [0, 1],
-    [1, 1],
-    [1, 2],
-    [1, 3]
-];
-var activePosition = [5, 10];
 document.onkeydown = function (evt) {
     var evt = evt || window.event;
     switch (evt.keyCode) {
@@ -50,18 +45,6 @@ document.onkeydown = function (evt) {
     console.log(activePosition[1]);
     drawCanvas();
 };
-droppedCells.push([0, 0]);
-droppedCells.push([1, 0]);
-droppedCells.push([2, 0]);
-droppedCells.push([3, 0]);
-droppedCells.push([4, 0]);
-droppedCells.push([5, 0]);
-droppedCells.push([6, 0]);
-droppedCells.push([7, 0]);
-droppedCells.push([7, 1]);
-droppedCells.push([9, 0]);
-droppedCells.push([9, 1]);
-droppedCells.push([9, 2]);
 function drawCanvas() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.save();
@@ -77,7 +60,7 @@ function drawCanvas() {
 }
 function canMove(figure, position) {
     if (figure.filter(function (item, index) {
-        if (position[0] + item[0] >= width || position[0] + item[0] < 0 || position[1] + item[1] >= heigth || position[1] + item[1] < 0)
+        if (position[0] + item[0] >= width || position[0] + item[0] < 0 || position[1] + item[1] < 0)
             return true;
         return false;
     }).length > 0)
@@ -102,7 +85,152 @@ function rotateCW(figure, orientation) {
 function rotateCCW(figure, orientation) {
     return figure;
 }
+function getNextPosition(orientation) {
+    switch (orientation) {
+        case 2 /* Down */: return 3 /* Left */;
+        case 3 /* Left */: return 0 /* Up */;
+        case 0 /* Up */: return 1 /* Right */;
+        case 1 /* Right */: return 2 /* Down */;
+    }
+}
+function getFigure(figure, orientation) {
+    switch (figure) {
+        case 1 /* BackEl */:
+            switch (orientation) {
+                case 0 /* Up */:
+                    return [[0, 2], [0, 3], [1, 1], [2, 1]];
+                case 1 /* Right */:
+                    return [[1, 1], [1, 2], [1, 3], [2, 3]];
+                case 2 /* Down */:
+                    return [[0, 2], [1, 2], [2, 2], [2, 1]];
+                case 3 /* Left */:
+                    return [[0, 1], [1, 1], [1, 2], [1, 3]];
+            }
+            break;
+        case 5 /* BackGee */:
+            switch (orientation) {
+                case 0 /* Up */:
+                    return [[1, 1], [1, 2], [2, 2], [2, 3]];
+                case 1 /* Right */:
+                    return [[1, 2], [2, 1], [2, 2], [3, 1]];
+                case 2 /* Down */:
+                    return [[1, 1], [1, 2], [2, 2], [2, 3]];
+                case 3 /* Left */:
+                    return [[1, 2], [2, 1], [2, 2], [3, 1]];
+            }
+            break;
+        case 2 /* Cube */:
+            switch (orientation) {
+                case 0 /* Up */:
+                    return [[1, 1], [1, 2], [2, 1], [2, 2]];
+                case 1 /* Right */:
+                    return [[1, 1], [1, 2], [2, 1], [2, 2]];
+                case 2 /* Down */:
+                    return [[1, 1], [1, 2], [2, 1], [2, 2]];
+                case 3 /* Left */:
+                    return [[1, 1], [1, 2], [2, 1], [2, 2]];
+            }
+            break;
+        case 0 /* El */:
+            switch (orientation) {
+                case 0 /* Up */:
+                    return [[0, 2], [1, 2], [2, 2], [2, 3]];
+                case 1 /* Right */:
+                    return [[1, 1], [1, 2], [1, 3], [2, 1]];
+                case 2 /* Down */:
+                    return [[0, 2], [0, 3], [1, 3], [2, 3]];
+                case 3 /* Left */:
+                    return [[1, 3], [2, 3], [2, 2], [2, 1]];
+            }
+            break;
+        case 4 /* Gee */:
+            switch (orientation) {
+                case 0 /* Up */:
+                    return [[1, 3], [1, 2], [2, 2], [2, 1]];
+                case 1 /* Right */:
+                    return [[0, 1], [1, 1], [1, 2], [2, 2]];
+                case 2 /* Down */:
+                    return [[1, 3], [1, 2], [2, 2], [2, 1]];
+                case 3 /* Left */:
+                    return [[0, 1], [1, 1], [1, 2], [2, 2]];
+            }
+            break;
+        case 6 /* Piramide */:
+            switch (orientation) {
+                case 0 /* Up */:
+                    return [[1, 1], [2, 1], [2, 2], [3, 1]];
+                case 1 /* Right */:
+                    return [[1, 1], [1, 2], [1, 3], [2, 2]];
+                case 2 /* Down */:
+                    return [[1, 2], [2, 2], [3, 2], [2, 1]];
+                case 3 /* Left */:
+                    return [[2, 1], [2, 2], [2, 2], [1, 2]];
+            }
+            break;
+        case 3 /* Stick */:
+            switch (orientation) {
+                case 0 /* Up */:
+                    return [[1, 0], [1, 1], [1, 2], [1, 3]];
+                case 1 /* Right */:
+                    return [[0, 1], [1, 1], [1, 1], [3, 1]];
+                case 2 /* Down */:
+                    return [[1, 0], [1, 1], [1, 2], [1, 3]];
+                case 3 /* Left */:
+                    return [[0, 1], [1, 1], [1, 1], [3, 1]];
+            }
+            break;
+    }
+    return [[0, 1], [1, 1], [1, 1], [3, 1]];
+}
+document.write("<canvas id='mainCanvas' width='400px' height='800px '  style='border:1px solid #000000;'></canvas>");
+var canvas = document.getElementById("mainCanvas");
+var context = canvas.getContext("2d");
+var cellSize = 40;
+var width = 10;
+var heigth = 20;
+var tick = 1000;
+var droppedCells = [];
+var activeFigure = null;
+var nextFigure = null;
+var activePosition = [5, 10];
+function getRandomArbitrary(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+function getFigureOnTheField(figure, position) {
+    return figure.map(function (item) {
+        return [item[0] + position[0], item[1] + position[1]];
+    });
+}
+droppedCells.push([0, 0]);
+droppedCells.push([1, 0]);
+droppedCells.push([2, 0]);
+droppedCells.push([3, 0]);
+droppedCells.push([4, 0]);
+droppedCells.push([5, 0]);
+droppedCells.push([6, 0]);
+droppedCells.push([7, 0]);
+droppedCells.push([7, 1]);
+droppedCells.push([9, 0]);
+droppedCells.push([9, 1]);
+droppedCells.push([9, 2]);
+newFigure();
+function newFigure() {
+    activePosition = [4, 18];
+    if (nextFigure == null) {
+        nextFigure = getFigure(getRandomArbitrary(0, 6), 0 /* Up */);
+    }
+    activeFigure = nextFigure;
+    nextFigure = getFigure(getRandomArbitrary(0, 6), 0 /* Up */);
+}
 setInterval(function () {
+    var positionLower = [activePosition[0], activePosition[1] - 1];
+    if (!canMove(activeFigure, positionLower)) {
+        droppedCells = droppedCells.concat(getFigureOnTheField(activeFigure, activePosition));
+        newFigure();
+    }
+    else {
+        activePosition = [activePosition[0], activePosition[1] - 1];
+    }
     drawCanvas();
 }, tick);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,33 +1,23 @@
-enum Orientation {Up, Right, Down, Left}
+enum Orientation {
+    Up,
+    Right,
+    Down,
+    Left
+}
+
+enum Figure{
+ El,
+    BackEl,
+    Cube,
+    Stick,
+    Gee,
+    BackGee,
+    Piramide
+}
 
 class Figures{
 
 }
-
-document.write("<canvas id='mainCanvas' width='400px' height='800px '  style='border:1px solid #000000;'></canvas>");
-
-var canvas = <HTMLCanvasElement> document.getElementById("mainCanvas")
-
-var context = canvas.getContext("2d")
-
-var cellSize: number = 40
-
-var width: number = 10
-
-var heigth: number = 20
-
-var tick: number = 1000
-
-var droppedCells:  Array< [number, number]> = []
-
-var activeFigure:  Array< [number, number]> = [
-    [0,1],
-    [1,1],
-    [1,2],
-    [1,3]
-]
-
-var activePosition : [number, number] = [5,10]
 
 document.onkeydown = (evt)=> {
     var evt = evt || window.event
@@ -62,21 +52,6 @@ document.onkeydown = (evt)=> {
     drawCanvas()
 };
 
-
-droppedCells.push([0,0])
-droppedCells.push([1,0])
-droppedCells.push([2,0])
-droppedCells.push([3,0])
-droppedCells.push([4,0])
-droppedCells.push([5,0])
-droppedCells.push([6,0])
-droppedCells.push([7,0])
-droppedCells.push([7,1])
-droppedCells.push([9,0])
-droppedCells.push([9,1])
-droppedCells.push([9,2])
-
-
 function drawCanvas(){
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.save()
@@ -95,50 +70,243 @@ function drawCanvas(){
 }
 
 function canMove(figure: Array< [number, number]>, position: [number, number]) : boolean {
-
     if (figure.filter((item, index) => {
 
             if (position[0] + item[0] >= width ||
                 position[0] + item[0] < 0 ||
-                position[1] + item[1] >= heigth ||
+                //position[1] + item[1] >= heigth ||
                 position[1] + item[1] < 0) return true;
-
             return false
-
         }).length > 0) return false
-
 
     if (figure.filter((item, index) => {
-
-            if (droppedCells.filter((cell, index)=>{
-
+            if (droppedCells.filter((cell, index) => {
                     if (cell[0] == position[0] + item[0] && cell[1] == position[1] + item[1]) return true
-                    else return false;
-
+                    else return false
                 }).length > 0) return true;
-
             else return false
-
         }).length > 0) return false
-
     return true
 }
-
 
 function rotateCW (figure: Array< [number, number]>, orientation: Orientation) :  Array< [number, number]> {
     return figure;
 }
 
-
 function rotateCCW (figure: Array< [number, number]>, orientation: Orientation) : Array< [number, number]> {
     return figure;
 }
 
+function getNextPosition(orientation: Orientation){
+
+    switch (orientation){
+        case Orientation.Down: return Orientation.Left
+        case Orientation.Left: return Orientation.Up;
+        case  Orientation.Up: return Orientation.Right;
+        case Orientation.Right: return Orientation.Down;
+    }
+}
+
+function  getFigure(figure: Figure, orientation: Orientation ): Array<[number, number]>{
+
+    switch (figure){
+        case Figure.BackEl:
+            switch (orientation){
+
+                case Orientation.Up:
+                    return [[0,2],[0,3],[1,1],[2,1]];
+                case Orientation.Right:
+                    return [[1,1],[1,2],[1,3],[2,3]];
+
+                case Orientation.Down:
+                    return [[0,2],[1,2],[2,2],[2,1]];
+                case Orientation.Left:
+                    return [[0,1],[1,1],[1,2],[1,3]];
+            }
+            break;
+        case Figure.BackGee:
+            switch (orientation){
+
+                case Orientation.Up:
+                    return [[1,1],[1,2],[2,2],[2,3]];
+                case Orientation.Right:
+                    return [[1,2],[2,1],[2,2],[3,1]];
+                case Orientation.Down:
+                    return [[1,1],[1,2],[2,2],[2,3]];
+                case Orientation.Left:
+                    return [[1,2],[2,1],[2,2],[3,1]];
+            }
+            break;
+        case Figure.Cube:
+            switch (orientation){
+
+                case Orientation.Up:
+                    return [[1,1],[1,2],[2,1],[2,2]];
+                case Orientation.Right:
+                    return [[1,1],[1,2],[2,1],[2,2]];
+
+                case Orientation.Down:
+                    return [[1,1],[1,2],[2,1],[2,2]];
+
+                case Orientation.Left:
+                    return [[1,1],[1,2],[2,1],[2,2]];
+
+            }
+            break;
+        case Figure.El:
+            switch (orientation){
+
+                case Orientation.Up:
+                    return [[0,2],[1,2],[2,2],[2,3]];
+
+                case Orientation.Right:
+                    return [[1,1],[1,2],[1,3],[2,1]];
+
+                case Orientation.Down:
+                    return [[0,2],[0,3],[1,3],[2,3]];
+
+                case Orientation.Left:
+                    return [[1,3],[2,3],[2,2],[2,1]];
+
+            }
+            break;
+
+        case Figure.Gee:
+            switch (orientation){
+
+                case Orientation.Up:
+                    return [[1,3],[1,2],[2,2],[2,1]];
+
+                case Orientation.Right:
+                    return [[0,1],[1,1],[1,2],[2,2]];
+
+                case Orientation.Down:
+                    return [[1,3],[1,2],[2,2],[2,1]];
+
+                case Orientation.Left:
+                    return [[0,1],[1,1],[1,2],[2,2]];
+
+            }
+            break;
 
 
+        case Figure.Piramide:
+            switch (orientation){
+
+                case Orientation.Up:
+                    return [[1,1],[2,1],[2,2],[3,1]];
+
+                case Orientation.Right:
+                    return [[1,1],[1,2],[1,3],[2,2]];
+
+                case Orientation.Down:
+                    return [[1,2],[2,2],[3,2],[2,1]];
+
+                case Orientation.Left:
+                    return [[2,1],[2,2],[2,2],[1,2]];
+
+            }
+            break;
+
+        case Figure.Stick:
+            switch (orientation){
+
+                case Orientation.Up:
+                    return [[1,0],[1,1],[1,2],[1,3]];
+
+                case Orientation.Right:
+                    return [[0,1],[1,1],[1,1],[3,1]];
+
+                case Orientation.Down:
+                    return [[1,0],[1,1],[1,2],[1,3]];
+
+                case Orientation.Left:
+                    return [[0,1],[1,1],[1,1],[3,1]];
+
+            }
+            break;
+
+    }
+
+    return [[0,1],[1,1],[1,1],[3,1]];
+
+}
+
+document.write("<canvas id='mainCanvas' width='400px' height='800px '  style='border:1px solid #000000;'></canvas>")
+
+var canvas = <HTMLCanvasElement> document.getElementById("mainCanvas")
+
+var context = canvas.getContext("2d")
+
+var cellSize: number = 40
+
+var width: number = 10
+
+var heigth: number = 20
+
+var tick: number = 1000
+
+var droppedCells:  Array< [number, number]> = []
+
+var activeFigure:  Array< [number, number]> = null
+
+var nextFigure:  Array< [number, number]> = null
+
+var activePosition : [number, number] = [5,10]
+
+function getRandomArbitrary(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function getFigureOnTheField(figure: Array<[number, number]>, position: [number, number]) : Array<[number, number]>{
+    return figure.map((item)=>{
+        return <[number, number]>[item[0] + position[0], item[1] + position[1]]
+    });
+}
+
+    droppedCells.push([0, 0])
+    droppedCells.push([1, 0])
+    droppedCells.push([2, 0])
+    droppedCells.push([3, 0])
+    droppedCells.push([4, 0])
+    droppedCells.push([5, 0])
+    droppedCells.push([6, 0])
+    droppedCells.push([7, 0])
+
+    droppedCells.push([7, 1])
+    droppedCells.push([9, 0])
+    droppedCells.push([9, 1])
+    droppedCells.push([9, 2])
+
+      newFigure()
+
+function newFigure() :void {
+    activePosition = [4, 18]
+
+    if (nextFigure == null){
+
+        nextFigure = getFigure(
+            <Figure> getRandomArbitrary(0,6),
+            Orientation.Up
+        )
+    }
+
+    activeFigure = nextFigure
+
+    nextFigure = getFigure(
+        <Figure> getRandomArbitrary(0,6),
+        Orientation.Up)
+}
 
 setInterval(()=>{
-
-        drawCanvas()
-
-    },tick);
+    var positionLower = <[number, number]>[activePosition[0], activePosition[1]-1]
+    if (!canMove(activeFigure, positionLower))
+    {
+        droppedCells = droppedCells.concat(getFigureOnTheField(activeFigure, activePosition))
+        newFigure()
+    }
+    else{
+        activePosition = [activePosition[0], activePosition[1]-1]
+    }
+    drawCanvas()
+},tick);
